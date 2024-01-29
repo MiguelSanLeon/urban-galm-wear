5ff06345bf833733db2b785c7936213f8fdea9c7
MiguelSanLeon <>
2024-01-29 12:24:30 +0000

    Added sorting to product page

--- products/views.py
+++ products/views.py
@@ -10,38 +10,52 @@ def search_products(query):
 
 
 def all_products(request):
-    """ A view to show all products, sorting and search queries """
+    """ A view to show all products, including sorting and search queries """
 
-    query = request.GET.get('q')
-    category_query = request.GET.get('category')
-    sort_by = request.GET.get('sort', 'name')
-    direction = request.GET.get('direction', 'asc')
-
-    categories = None
     products = Product.objects.all()
+    query = None
+    categories = None
+    sort = None
+    direction = None
 
-    if category_query:
-        categories = category_query.split(',')
-        products = products.filter(category__name__in=categories)
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
 
-    if query:
-        products = search_products(query)
-        if not products.exists():
-            messages.warning(
-                request, "No products match your search criteria.")
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
-    if sort_by in ['name', 'price', 'rating', 'carbon_footprint']:
-        if direction == 'asc':
-            products = products.order_by(sort_by)
-        else:
-            products = products.order_by(f'-{sort_by}')
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(
+                    request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(
+                description__icontains=query)
+            products = products.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
-        'sort_by': sort_by,
-        'direction': direction,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
