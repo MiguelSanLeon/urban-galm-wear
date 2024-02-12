a1bb4889e9ffd9b6b1b946cc0c0292153b6a7fde
MiguelSanLeon Leon <maslemvz@gmail.com>
2024-02-12 12:55:50 +0000

    Added stripe elements.js

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template =  'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51OJyeGC3dSou0Fp0AHYMauPta0Nzvgn7rWngUIzyqb2ESlR0uX7AQpncmWTjj0toS0AE5bz71QzpDzZfRWnZ0l5P00MP1bHATC',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
