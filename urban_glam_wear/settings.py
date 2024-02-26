2775bb4961a3d49e189b3bb901abb2b464614a38
MiguelSanLeon Leon <maslemvz@gmail.com>
2024-02-26 14:19:34 +0000

    Fixed typo in settings.py custom_storages

--- urban_glam_wear/settings.py
+++ urban_glam_wear/settings.py
@@ -191,9 +191,9 @@ if 'USE_AWS' in os.environ:
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
-    STATICFILES_STORAGE = 'custom_storage.StaticStorage'
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
-    DEFAULT_FILES_STORAGE = 'custom_storage.MediaStorage'
+    DEFAULT_FILES_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
 
     # Override static and media Urls in production
