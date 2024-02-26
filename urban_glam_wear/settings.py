b5520ddd853a75ebeca5abb188794d22dcd9037e
MiguelSanLeon Leon <maslemvz@gmail.com>
2024-02-26 11:33:40 +0000

    deployment test2

--- urban_glam_wear/settings.py
+++ urban_glam_wear/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-&vvenxyr!+p++@bemxrj%j4o+ckbr+p&anphnw=qk1#zvpb_v!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '8000-miguelsanle-urbangalmwe-s3mlzyjzbhe.ws-eu108.gitpod.io',]
+ALLOWED_HOSTS = ['localhost', '8000-miguelsanle-urbangalmwe-s3mlzyjzbhe.ws-eu108.gitpod.io','urban-glam-wear-e8c11a4943c2.herokuapp.com']
 
 
 # Application definition
