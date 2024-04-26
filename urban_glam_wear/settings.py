ca8b3056b95d0beb9a30f6cb8774283c2a30b69d
MiguelSanLeon Leon <maslemvz@gmail.com>
2024-04-26 11:25:28 +0000

    Added categories and products to database and create wishlist app

--- urban_glam_wear/settings.py
+++ urban_glam_wear/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'testkey')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '8000-miguelsanle-urbangalmwe-s3mlzyjzbhe.ws-eu108.gitpod.io','urban-glam-wear-e8c11a4943c2.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '8000-miguelsanle-urbangalmwe-164zg1zisnn.ws-eu110.gitpod.io','urban-glam-wear-e8c11a4943c2.herokuapp.com']
 
 
 # Application definition
