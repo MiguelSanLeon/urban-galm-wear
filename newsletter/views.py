e4852d3ec6596ec7f2f33d475725adc34b7ce046
MiguelSanLeon Leon <maslemvz@gmail.com>
2024-05-05 09:27:03 +0000

    Added toast message when email is already subscribed

--- newsletter/views.py
+++ newsletter/views.py
@@ -4,35 +4,35 @@ from django.core.mail import send_mail
 from .forms import SubscriptionForm
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
-
+from .models import Subscriber
 
 def subscribe(request):
     form = SubscriptionForm()
     if request.method == 'POST':
         form = SubscriptionForm(request.POST)
         if form.is_valid():
-            subscriber = form.save()
+            email = form.cleaned_data.get('email')
+            if Subscriber.objects.filter(email=email).exists():
+                messages.warning(request, 'This email is already subscribed.')
+            else:
+                subscriber = form.save()
+                subject_template = 'newsletter/email_subject.txt'
+                body_template = 'newsletter/email_body.html'
 
-            subject_template = 'newsletter/email_subject.txt'
-            body_template = 'newsletter/email_body.html'
+                subject = render_to_string(subject_template).strip()
+                body = render_to_string(body_template, {'subscriber': subscriber})
 
-            subject = render_to_string(subject_template).strip()
-            body = render_to_string(body_template, {'subscriber': subscriber})
+                send_mail(
+                    subject,
+                    strip_tags(body),
+                    'urbanglamwear@urb.com',
+                    [subscriber.email],
+                    fail_silently=False,
+                    html_message=body
+                )
 
-            send_mail(
-                subject,
-                strip_tags(body),
-                'urbanglamwear@urb.com',
-                [subscriber.email],
-                fail_silently=False,
-                html_message=body
-            )
-
-            messages.success(request, 'Thank you for subscribing to'
-                             ' our newsletter. You will receive a '
-                             'Welcome email soon.')
-
-            return redirect('home')
+                messages.success(request, 'Thank you for subscribing to our newsletter. You will receive a Welcome email soon.')
+                return redirect('home')
 
     context = {'form': form}
     return render(request, 'newsletter/subscribe.html', context)
